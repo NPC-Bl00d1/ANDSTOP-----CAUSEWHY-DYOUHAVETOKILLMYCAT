538d8d50219bfd02fefa4e6296dd8955a193fd19
Ishita L <79676329+NPC-Bl00d1@users.noreply.github.com>
2022-08-23 18:38:53 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var objects = [];
+status = "";
 video = "";
 
 function preload(){
@@ -19,6 +21,26 @@ function draw(){
 
     image(video, 0, 0, 480, 380);
 
+    if(status != ""){
+
+        objdetect.detect(video, gotResult);
+
+        for (i = 0; i < objects.length; i++){
+
+            document.getElementById("status").innerHTML = "Status: Object(s) detected!";
+            document.getElementById("numobject").innerHTML = "Number of Objects: " + objects.length;
+
+            fill("#ff0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+
+    }
+
 }
 
 function start(){
@@ -36,5 +58,17 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+
+}
+
+function gotResult(error, results){
+
+    if(error){
+
+        console.log(error);
+
+    }
+    console.log(results);
+    objects = results;
 
 }
